b25274c1e2e14c564c66758817c673f2f7d71367
shub0 <shubo.liu.mail@gmail.com>
2016-09-01 21:20:06 -0700

    Code review Commit

--- notebooks/Users/shubo@conviva.com/github.scala.scala
+++ notebooks/Users/shubo@conviva.com/github.scala.scala
@@ -1,5 +1,9 @@
-// Databricks notebook source exported at Fri, 2 Sep 2016 04:13:56 UTC
+// Databricks notebook source exported at Fri, 2 Sep 2016 04:20:15 UTC
 println("Test DBC Integration with Github. ")
+
+// COMMAND ----------
+
+println("Now let us test code review")
 
 // COMMAND ----------
 
